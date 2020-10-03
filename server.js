4bc7bc2ace560b8b2cfbca3387559c65fe64964f
Sunber2111 <namdang3979@gmail.com>
2020-10-03 14:04:08 +0700

    final

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const connectionDb = require("./config/connectDb");
 require("dotenv").config();
 
 let app = express();
-app.use(cors());
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
+
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
